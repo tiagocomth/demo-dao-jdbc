e9f2299958c5f9f7d5eef024ef36316af5aab59a
Thiago <thiago8jesusaraujo@gmail.com>
2024-12-04 12:03:10 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,10 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+	public static void main(String[] args) {
+		Department dep = new Department(1, "Programação");
+		System.out.println(dep);
+	}
+}

d25db49ada74fba95ea7e13f00179719f904b220
Thiago <thiago8jesusaraujo@gmail.com>
2024-12-05 14:11:04 -0300

    delete implementation and SellerDaoJDBC and DaoFactory implements

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,8 @@ import model.entities.Seller;
 public class Program {
 	public static void main(String[] args) {
 	
+		Scanner sc = new Scanner(System.in);
+		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("==== TEST 1: seller FindById ====");
@@ -38,5 +41,15 @@ public class Program {
 		seller.setName("Neymar Junior");
 		sellerDao.update(seller);
 		System.out.println("Update Completed!");
+		
+		System.out.println("\n==== TEST 5: seller DeleteById ====");
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete Completed");
+		
+		
+		sc.close();
+		
 	}
 }
